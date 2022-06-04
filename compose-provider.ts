50cf7abc1efcd8fdad05b1c28dd8b59848051405
Alfred Cepeda <alfred@cepeda.io>
2022-06-04 00:14:12 -0400

    Fixes composer provider to newest cli version

--- compose-provider.ts
+++ compose-provider.ts
@@ -1,51 +1,26 @@
-import {ComposeProvider} from "@vlegm/cli";
+import {NodeJSService, NodeJSSource, PGAdminService, PostgresService} from "@vlegm/cli";
 
-export default {
-  version: "3.7",
-  predefined: ["postgres", "redis", "pgadmin"],
-  env: {
-    POSTGRES_HOST: 'postgres',
-    POSTGRES_PORT: '5432',
-    POSTGRES_DB: 'event-matcher',
-    POSTGRES_USER: 'superuser',
-    POSTGRES_PASSWORD: 'password',
-    PGADMIN_DEFAULT_EMAIL: 'superuser@local.com',
-    PGADMIN_DEFAULT_PASSWORD: 'password',
-    SERVER_PORT: 8080
-  },
-  services: {
-    client: {
-      repo: {
-        url: "git@github.com:vlegm/event-matcher-client.git",
-        init: "yarn install"
-      },
-      ports: [
-        '3000:3000'
-      ],
-      command: [
-        "yarn install",
-        "yarn dev",
-      ],
-      image: "vlegm/dev-alpine:latest"
-    },
-    server: {
-      repo: {
-        url: "git@github.com:vlegm/event-matcher-cloud-functions.git",
-        init: "yarn install"
-      },
-      ports: [
-        "8080:8080"
-      ],
-      tty: true,
-      mnts: ["expressman:@vlegm/expressman"],
-      command: "yarn dev",
-      image: "vlegm/dev-alpine:latest"
-    },
-    expressman: {
-      repo: {
-        url: "git@github.com:vlegm/expressman.git",
-        init: "yarn install"
-      }
-    }
-  }
-} as ComposeProvider;
+export const server = new NodeJSService(8080);
+server.addSource("git@github.com:vlegm/event-matcher-server.git", "yarn install");
+
+export const client = new NodeJSService(3000);
+client.addSource("git@github.com:vlegm/event-matcher-client.git", "yarn install");
+
+export const expressman = new NodeJSSource("git@github.com:vlegm/expressman.git");
+
+export const postgres = new PostgresService({
+  POSTGRES_HOST: 'postgres',
+  POSTGRES_PORT: 5432,
+  POSTGRES_DB: 'event-matcher',
+  POSTGRES_USER: 'superuser',
+  POSTGRES_PASSWORD: 'password',
+}, {
+  port: 5432
+})
+
+export const pgadmin = new PGAdminService({
+  PGADMIN_DEFAULT_EMAIL: 'superuser@local.com',
+  PGADMIN_DEFAULT_PASSWORD: 'password',
+}, {
+  port: 8081
+})
